7f7097ba2afca50d62fe924f5ea18eb68cbda483
David Gumberg <davidzgumberg@gmail.com>
2023-10-06 02:15:48 -0400

    Add partial date class and methods for parsing OL strings into partial dates. Also, add timeout settings for OpenURI

--- lib/fetchworks.rb
+++ lib/fetchworks.rb
@@ -1,16 +1,45 @@
 # frozen_string_literal: true
 
+OPENURI_OPTS = {
+  read_timeout: 10,
+  open_timeout: 10
+}.freeze
+
 require_relative "fetchworks/version"
+require_relative "fetchworks/partial_date"
 
 require "json"
 require "open-uri"
+require "time"
 
-HOST = 'openlibrary.org'
-PATH = '/api/books'
-QUERY_POSTFIX = '&jscmd=data&format=json'
+HOST = "openlibrary.org"
+PATH = "/api/books"
+QUERY_POSTFIX = "&jscmd=data&format=json"
 
+# Mixin Module for classes with a hash attribute @data
+# That routes classinstance.method to classinstance.data[:method]
+module HashMethodable
+  # Expose hash members as methods
+  def method_missing(method_name, *_args, &_block)
+    key = method_name.to_s
+    @data.key?(key) ? @data[key] : nil
+  end
+
+  # Advertise hash member methods
+  def respond_to_missing?(method_name, include_private = false)
+    key = method_name.to_s
+    @data.key?(key) || super
+  end
+end
+
+# Provides methods for accessing OpenLibrary resources
 module OpenLibrary
+  # Declare error classes
   class InvalidISBN < StandardError; end
+  class OpenLibraryError < StandardError; end
+  class OLBadStatus < OpenLibraryError; end
+  class OLResourceNotFound < OpenLibraryError; end
+  class OLDateStrUnparseable < OpenLibraryError; end
 
   def self.get_book(isbn)
     raise InvalidISBN unless isbn.is_a?(Integer) || isbn.is_a?(String)
@@ -20,51 +49,147 @@ module OpenLibrary
 
     # Primitive ISBN check: regex match for a 10 or 12 digit number
     # TODO: check prefix and checksum
-    raise InvalidISBN unless /\A\d{10}(\d{3})?\z/.match? isbn 
+    raise InvalidISBN unless /\A\d{10}(\d{3})?\z/.match? isbn
 
     query = "bibkeys=ISBN:#{isbn}#{QUERY_POSTFIX}"
     uri = URI::HTTPS.build(host: HOST, path: PATH, query: query)
-    JSON.parse(uri.open.read).values.first
-  end
+    response = uri.open(**OPENURI_OPTS)
 
-  def method_missing(method_name, *args, &block)
-    key = method_name.to_s
-    @data.key?(key) ? @data[key] : nil
-  end
+    if response&.status != ["200", "OK"]
+      raise OLBadStatus, response&.status
+    end
 
-  def respond_to_missing?(method_name, include_private = false)
-    key = method_name.to_s
-    @data.key?(key) || super
+    JSON.parse(response.read).values.first
   end
 
   def self.get_author(url)
-    url = url[%r{\A.*(?=/)}] + '.json'
+    url = url[%r{\A.*(?=/)}] + ".json"
     uri = URI.parse(url)
-    p uri
     JSON.parse(uri.open.read)
+  end
+
+  # Returns a PartialDate from strings of one of the following four formats:
+  # "YYYY" | "Month YYYY" | "Month DD, YYYY" | "DD Month, YYYY"
+  #
+  # "Month"s are abbreviated usually, but sometimes not.
+  #
+  # This gets nasty, because we have a number of formats when two
+  # or three elements are present. We decide which one based off
+  # of which member is a recognized month name.
+  def self.partial_date_from_str(str)
+    return [] unless str.is_a?(String) && !str.empty?
+
+    # Replace commas with spaces and part out the date string
+    split = str.tr(",", " ").split
+
+    PartialDate.new(
+      case split.length
+      # e.g. "1991"
+      when 1
+        [split[0].to_i]
+
+      when 2
+        # e.g. "Jan 1991"
+        if Date::ABBR_MONTHNAMES.include?(split[0])
+          [split[1].to_i,
+           Date::ABBR_MONTHNAMES.index(split[0])]
+
+        # e.g. "January 1991"
+        elsif Date::MONTHNAMES.include?(split[0])
+          [split[1].to_i,
+           Date::MONTHNAMES.index(split[0])]
+        else
+          raise OLDateStrUnparseable
+        end
+
+      when 3
+        # e.g. "Jan 12, 1991"
+        if Date::ABBR_MONTHNAMES.include?(split[0])
+          [split[2].to_i,
+           Date::ABBR_MONTHNAMES.index(split[0]),
+           split[1].to_i]
+
+        # e.g. "January 12, 1991"
+        elsif Date::MONTHNAMES.include?(split[0])
+          [split[2].to_i,
+           Date::MONTHNAMES.index(split[0]),
+           split[1].to_i]
+
+        # e.g. "12 Jan 1991"
+        elsif Date::ABBR_MONTHNAMES.include?(split[1])
+          [split[2].to_i,
+           Date::ABBR_MONTHNAMES.index(split[1]),
+           split[0].to_i]
+
+        # e.g. "12 January 1991"
+        elsif Date::MONTHNAMES.include?(split[1])
+          [split[2].to_i,
+           Date::MONTHNAMES.index(split[1]),
+           split[0].to_i]
+        end
+      end
+    )
   end
 end
 
-class OpenLibraryBook
-  include OpenLibrary
+# Methods common to OpenLibraryBook's and OpenLibraryAuthor's
+# Exposes the JSON representation of an OL entry as a hash
+# through @data, and as methods with method_missing,
+class OpenLibraryEntry
+  include HashMethodable
 
   attr_reader :data
 
+  def initialize(data)
+    raise StandardError unless data.is_a? Hash
+
+    @data = data
+  end
+end
+
+# A class that represents authors in the OpenLibrary database
+class OpenLibraryAuthor < OpenLibraryEntry
+  # Can only access authors at present via
+  # the URL provided for them in book data
+  def initialize(url)
+    super OpenLibrary.get_author(url)
+  end
+
+  def birth_date
+    str = @data["birth_date"]
+    OpenLibrary.partial_date_from_str(str)
+  end
+
+  def death_date
+    str = @data["death_date"]
+    OpenLibrary.partial_date_from_str(str)
+  end
+end
+
+# A class that represents books in the OpenLibrary database
+# It exposes the JSON representation that OL provides through their API
+# as a hash through @data, and as methods with method_missing,
+# and handles some comple
+class OpenLibraryBook < OpenLibraryEntry
   # Identifier is assumed to be ISBN10/ISBN13
   # TODO: Add a check or identification type
   def initialize(ident)
-    @data = OpenLibrary.get_book(ident)
+    super OpenLibrary.get_book(ident)
+  end
+
+  def publish_date
+    str = @data["publish_date"]
+    OpenLibrary.partial_date_from_str(str)
   end
 
   def authors_details
     authors.collect do |author|
-      p author["url"]
-      OpenLibrary.get_author(author["url"])
+      OpenLibraryAuthor.new author["url"]
     end
   end
 end
 
-module Fetchworks
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# module Fetchworks
+#  class Error < StandardError; end
+#  # Your code goes here...
+# end
---
+++ lib/fetchworks/partial_date.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'time'
+
+# Date representation that permits nil values
+# We need this because of the nature of date values for works
+# Oftentimes a year is known, but a month, or day is not.
+class PartialDate
+  class PartialDateError < StandardError; end
+  class PartialDateArgError < PartialDateError; end
+  attr_accessor :year, :month, :day
+
+  def date
+    Date.new(year, (month || 1), (day || 1))
+  end
+
+  def time
+    date.to_time
+  end
+
+  def ==(other)
+    self.class == other.class &&
+      @year == other.year   &&
+      @month == other.month &&
+      @day == other.day
+  end
+
+  # We accept three formats, Date, Time (which we drop down to a Date),
+  # and an array of up three integers in the format: [year, month, day]
+  # TODO: Validate array input
+  def initialize(date)
+    if date.is_a? Time
+      date = date.to_date
+    end
+
+    if date.is_a? Date
+      date = [date.year, date.month, date.day]
+    end
+
+    unless date.is_a? Array
+      raise PartialDateArgError, "Did not receive a Date, Time, or Array"
+    end
+
+    unless date.length.between?(1, 3)
+      raise PartialDateArgError, "Received array of incorrect length."
+    end
+
+    @year = date[0]
+    @month = date[1]
+    @day = date[2]
+  end
+end
