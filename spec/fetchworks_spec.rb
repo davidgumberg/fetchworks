446f288f881f1e8d976216656a598f263c3703de
David Gumberg <davidzgumberg@gmail.com>
2023-10-06 02:17:00 -0400

    Add RSpec for OpenLibrary#partial_date_from_str

--- spec/fetchworks_spec.rb
+++ spec/fetchworks_spec.rb
@@ -39,4 +39,33 @@ RSpec.describe OpenLibrary do
       }.to raise_error OpenLibrary::InvalidISBN
     end
   end
+  context "when parsing OpenLibrary date strings" do
+    it "returns an empty array when passed nil" do
+      expect(OpenLibrary.partial_date_from_str(nil)).to eq([])
+    end
+    it "returns an empty array when passed something that isn't a string" do
+      expect(OpenLibrary.partial_date_from_str(Object.new)).to eq([])
+    end
+    it "returns the correct partial date for: YYYY" do
+      expect(OpenLibrary.partial_date_from_str "1901").to eq(PartialDate.new([1901]))
+    end
+    it "returns the correct partial date for: 'Month YYYY'" do
+      expect(OpenLibrary.partial_date_from_str "February 1901").to eq(PartialDate.new([1901, 2]))
+    end
+    it "returns the correct partial date for 'AbbrMonth YYYY" do
+      expect(OpenLibrary.partial_date_from_str "Feb 1901").to eq(PartialDate.new([1901, 2]))
+    end
+    it "returns the correct partial date for: 'Month DD, YYYY'" do
+      expect(OpenLibrary.partial_date_from_str "February 20, 1901").to eq(PartialDate.new([1901, 2, 20]))
+    end
+    it "returns the correct partial date for: 'AbbrMonth DD, YYYY'" do
+      expect(OpenLibrary.partial_date_from_str "Feb 20, 1901").to eq(PartialDate.new([1901, 2, 20]))
+    end
+    it "returns the correct partial date for: 'DD Month YYYY'" do
+      expect(OpenLibrary.partial_date_from_str "20 February 1901").to eq(PartialDate.new([1901, 2, 20]))
+    end
+    it "returns the correct partial date for: 'DD AbbrMonth YYYY'" do
+      expect(OpenLibrary.partial_date_from_str "20 Feb 1901").to eq(PartialDate.new([1901, 2, 20]))
+    end
+  end
 end
